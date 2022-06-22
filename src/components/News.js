25e8b34585a3712c27ce7a6174d4bc65c82258be
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-06-22 13:23:02 +0530

    update: filtered buttons for individual country

--- src/components/News.js
+++ src/components/News.js
@@ -7,7 +7,7 @@ const News = () => {
   const fetchNews = () => {
     axios
       .get(
-        "https://newsapi.org/v2/top-headlines?country=in&apiKey=5804eeaec0504c818cc3508707b6cbb2"
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=5804eeaec0504c818cc3508707b6cbb2"
       )
       .then((response) => {
         setFetchedNews(response.data.articles);
@@ -17,8 +17,17 @@ const News = () => {
 
   return (
     <>
-      <button className="btn btn-info" onClick={fetchNews}>
-        Fetch News
+      <button className="btn btn-info m-2" onClick={fetchNews}>
+        India News
+      </button>
+      <button className="btn btn-info m-2" onClick={fetchNews}>
+        US News
+      </button>
+      <button className="btn btn-info m-2" onClick={fetchNews}>
+        Canada News
+      </button>
+      <button className="btn btn-info m-2" onClick={fetchNews}>
+        Australia News
       </button>
       <div className="container border border-info">
         <div className="row">
@@ -26,10 +35,10 @@ const News = () => {
                 fetchedNews.map(value => {
                     return (
                         <div className="col border border-secondary m-2">
-                <img src={value.urlToImage} alt="news-image" style={{width: "20rem"}} className="border border-dark" />
+                <img src={value.urlToImage} alt="news-image" style={{width: "20rem"}} className="border border-dark mt-2" />
                 <h5 className="m-1 mark text-dark">{value.title}</h5>
                 <p>{value.description}</p>
-                <a href="#" className="btn btn-info m-3">Click Here!</a>
+                <a href={value.url} className="btn btn-info m-3">Click Here!</a>
             </div>
                     )
                 }
