6d5bb5b6052242f6c74bed24207c4667c49b1c7e
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-06-22 13:01:01 +0530

    fetch the news from NEWS API and render to the UI

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+
 const News = () => {
-    return (
-        <div>
-            <button className="btn btn-info">Fetch News</button>
+  const [fetchedNews, setFetchedNews] = useState([]);
+
+  const fetchNews = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=5804eeaec0504c818cc3508707b6cbb2"
+      )
+      .then((response) => {
+        setFetchedNews(response.data.articles);
+        // console.log(response)
+      });
+  };
+
+  return (
+    <>
+      <button className="btn btn-info" onClick={fetchNews}>
+        Fetch News
+      </button>
+      <div className="container border border-info">
+        <div className="row">
+            {
+                fetchedNews.map(value => {
+                    return (
+                        <div className="col border border-secondary m-2">
+                <img src={value.urlToImage} alt="news-image" style={{width: "20rem"}} className="border border-dark" />
+                <h5 className="m-1 mark text-dark">{value.title}</h5>
+                <p>{value.description}</p>
+                <a href="#" className="btn btn-info m-3">Click Here!</a>
+            </div>
+                    )
+                }
+                )
+            }
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default News;
